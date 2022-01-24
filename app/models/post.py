6a2d87be4fe8614d77e654c0ad8c3b70b5c1f204
PatrykB-dot <patrykfor3@gmail.com>
2022-01-24 09:19:49 +0100

    first commit

---
+++ app/models/post.py
@@ -0,0 +1,34 @@
+from mydatabase import Database
+
+
+class Post(object):
+    def __init__(self, blog_id, title, content, author, date, id):
+        self.blog_id = blog_id
+        self.title = title
+        self.content = content
+        self.author = author
+        self.date = date
+        self.id = id
+    
+    def json(self):
+        return {
+            'id': self.id,
+            'blog_id': self.blog_id,
+            'author': self.author,
+            'content': self.content,
+            'title': self.title,
+            'date': self.date,
+        }
+
+    def save_to_mongo(self):
+        Database.insert(collection='posts', data=self.json())
+
+    @staticmethod
+    def from_mongo(id):
+        return Database.find_one(collection='posts', query={ 'id': id })
+    #zwraca post z bazy danych
+
+    @staticmethod
+    def from_blog(id):
+        return [post for post in Database.find(collection='posts', query={ 'blog_id': id})]
+    #zwraca post z konkretnego bloga
